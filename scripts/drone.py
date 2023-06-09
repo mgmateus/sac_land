285d5beec76ae0003831434872ab02131e3d9417
mgmateus <matheus_gonsalvez@outlook.com>
2023-06-08 23:58:21 -0300

    Visualize inference

--- scripts/drone.py
+++ scripts/drone.py
@@ -45,8 +45,8 @@ class Hydrone:
         if data:
             rospy.logwarn('Inference')
             cv_inference = self.__img_process.image_transport(data)
-            cv2.imshow("Inference", cv_inference)
-            cv2.waitKey(3)
+            #cv2.imshow("Inference", cv_inference)
+            #cv2.waitKey(0)
         else:
             info = "Error in Bottom cam!"
             self.__info_pub.publish(info)
